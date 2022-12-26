70182aea29ab739900a8846b5877843e1bb76f6c
Fede <federico@freightos.com>
2022-12-26 23:35:19 +0100

    move script to html page to be sure nodeList is not comming empty

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,4 @@
-window.addEventListener('keydown', function (e) {
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-  //if(!audio) return //stop the function from running all toghether
-  audio.currentTime = 0 //rewind to the start
-  audio.play()
-  key.classList.add('playing')
-})
-
-function removeTransition(e) {
-  console.log(e)
-}
 
 
 
-let keys = document.querySelectorAll('.key')
-console.log(document.querySelectorAll('.key'))
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
 
