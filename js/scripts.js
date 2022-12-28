782cc63c0fc00211c443b9e4d6788aa4ba96ef83
Fede <federico@freightos.com>
2022-12-28 11:10:56 +0100

    scripts moved to src/script and audios to public

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+ const playSound = (e) => {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    if(!audio) return //stop the function from running all toghether
+    audio.currentTime = 0 //rewind to the start
+    audio.play()
+    key.classList.add('playing')
+  }
+
+  const removeTransition = (e) => {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+    key.classList.remove('playing')
+  }
+
+  window.addEventListener('keydown', playSound)
+  window.addEventListener('keyup', removeTransition)
 
 
 
