d1c352a800a1b69fd06a5ae496d2cf0767dba52b
Fede <federico@freightos.com>
2022-12-28 11:40:44 +0100

    set logic for avoid repetition of sound on continuous pulse

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
- const playSound = (e) => {
+
+let pulsed = false
+
+const playSound = (e) => {
+  if (!pulsed) {
+    pulsed = true
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    if(!audio) return //stop the function from running all toghether
+    if (!audio) return //stop the function from running all toghether
     audio.currentTime = 0 //rewind to the start
     audio.play()
     key.classList.add('playing')
   }
+}
 
-  const removeTransition = (e) => {
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
-    key.classList.remove('playing')
-  }
+const removeTransition = (e) => {
+  pulsed = false
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  key.classList.remove('playing')
+}
 
-  window.addEventListener('keydown', playSound)
-  window.addEventListener('keyup', removeTransition)
+window.addEventListener('keydown', playSound)
+window.addEventListener('keyup', removeTransition)
 
 
 
