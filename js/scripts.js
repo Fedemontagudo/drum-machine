78a30ae8d41aff4b1d0f4eb889cd82728b2ba201
Fede <federico@freightos.com>
2022-12-28 12:12:34 +0100

    finish exercice

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
 
-let pulsed = false
+let pulsed = []
 
 const playSound = (e) => {
-  if (!pulsed) {
-    pulsed = true
+  if (!pulsed[e.keyCode]) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if (!audio) return //stop the function from running all toghether
     audio.currentTime = 0 //rewind to the start
     audio.play()
+    pulsed[e.keyCode] = true
     key.classList.add('playing')
   }
 }
 
 const removeTransition = (e) => {
-  pulsed = false
   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if (!key) return
   key.classList.remove('playing')
+  pulsed[e.keyCode] = false
+
 }
 
 window.addEventListener('keydown', playSound)
 window.addEventListener('keyup', removeTransition)
-
-
-
-
